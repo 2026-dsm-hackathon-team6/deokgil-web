1dbba1d97ccf623ff7f0fbdeab2fe6585e3f3f34
Doeun <qwaszx080402@naver.com>
2026-07-14 16:56:19 +0900

    chore: 레거시 리다이렉트 기반 인증 콜백 페이지 제거

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,6 @@ const VenueMap = lazy(() => import("./pages/VenueMap"));
 const MyPage = lazy(() => import("./pages/MyPage"));
 const PastEvents = lazy(() => import("./pages/PastEvents"));
 const PastEventDetail = lazy(() => import("./pages/PastEventDetail"));
-const AuthCallback = lazy(() => import("./pages/AuthCallback"));
-const AuthError = lazy(() => import("./pages/AuthError"));
 const ProfileSetup = lazy(() => import("./pages/ProfileSetup"));
 const Notifications = lazy(() => import("./pages/Notifications"));
 const ReturnPlanner = lazy(() => import("./pages/ReturnPlanner"));
@@ -37,8 +35,6 @@ const AppRoutes = () => (
       <Route path="/mypage" element={<MyPage />} />
       <Route path="/mypage/past-events" element={<PastEvents />} />
       <Route path="/mypage/past-events/:id" element={<PastEventDetail />} />
-      <Route path="/auth/callback" element={<AuthCallback />} />
-      <Route path="/auth/error" element={<AuthError />} />
       <Route path="/profile/setup" element={<ProfileSetup />} />
       <Route path="/notifications" element={<Notifications />} />
       <Route path="/return-planner" element={<ReturnPlanner />} />
